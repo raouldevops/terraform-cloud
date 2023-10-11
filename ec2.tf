27a37fd3fd1a1284fad05e5fffb57d79994fbacb
Raoul Cipah <46892698+raouldevops@users.noreply.github.com>
2023-10-11 12:14:27 -0400

    remove tag

--- ec2.tf
+++ ec2.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "tf-cloud-ec2" {
   ami = "ami-067d1e60475437da2"
   instance_type = "t2.micro"
   key_name = "ec2-key-pair"
-  tags = {
-    Name = "ec2-tf-cloud"
-  }
+#  tags = {
+#     Name = "ec2-tf-cloud"
+#   }
   root_block_device {
     delete_on_termination = true
   }
