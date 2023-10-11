831af9f02a5b3d87c775fa2de76f9fbeb1cfbf39
Raoul Cipah <46892698+raouldevops@users.noreply.github.com>
2023-10-11 11:50:31 -0400

    Create ec2

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "tf-cloud-ec2" {
+  ami = "ami-067d1e60475437da2"
+  instance_type = "t2.micro"
+  key_name = "ec2-key-pair"
+  tags = {
+    Name = "ec2-tf-cloud"
+  }
+  root_block_device {
+    delete_on_termination = true
+  }
+}
